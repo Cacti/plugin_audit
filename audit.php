d0fdef05d1043ad2ef09938cecaa394d75489523
cigamit <jimmy@sqmail.org>
2017-02-26 04:32:01 -0600

    Initial Audit Event Log commit

---
+++ audit.php
@@ -0,0 +1,511 @@
+<?php
+/*
+ +-------------------------------------------------------------------------+
+ | Copyright (C) 2004-2017 The Cacti Group                                 |
+ |                                                                         |
+ | This program is free software; you can redistribute it and/or           |
+ | modify it under the terms of the GNU General Public License             |
+ | as published by the Free Software Foundation; either version 2          |
+ | of the License, or (at your option) any later version.                  |
+ |                                                                         |
+ | This program is distributed in the hope that it will be useful,         |
+ | but WITHOUT ANY WARRANTY; without even the implied warranty of          |
+ | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the           |
+ | GNU General Public License for more details.                            |
+ +-------------------------------------------------------------------------+
+ | Cacti: The Complete RRDTool-based Graphing Solution                     |
+ +-------------------------------------------------------------------------+
+ | This code is designed, written, and maintained by the Cacti Group. See  |
+ | about.php and/or the AUTHORS file for specific developer information.   |
+ +-------------------------------------------------------------------------+
+ | http://www.cacti.net/                                                   |
+ +-------------------------------------------------------------------------+
+*/
+
+chdir('../../');
+include_once('./include/auth.php');
+
+set_default_action();
+
+switch(get_request_var('action')) {
+case 'export':
+	audit_export_rows();
+
+	break;
+case 'purge':
+	audit_purge();
+
+	top_header();
+	audit_log();
+	bottom_footer();
+
+	break;
+case 'getdata':
+	$data = db_fetch_row_prepared('SELECT *
+		FROM audit_log 
+		WHERE id = ?',
+		array(get_filter_request_var('id')));
+
+	$output = '';
+
+	if (sizeof($data)) {
+		$attribs = json_decode($data['post']);
+
+		$nattribs = array();
+		foreach($attribs as $field => $content) {
+			$nattribs[$field] = $content;
+		}
+		ksort($nattribs);
+
+		if (sizeof($nattribs) > 16) {
+			$width = 'wide';
+		}else{
+			$width = 'narrow';
+		}
+
+		$output .= '<table style="width:100%" class="' . $width . '"><tr><td>';
+		$output .= '<span><b>' . __('Page:') . '</b>  <i>' . $data['page'] . '</i></span>';
+		$output .= '<br><span><b>' . __('User:') . '</b>  <i>' . get_username($data['user_id']) . '</i></span>';
+		$output .= '<br><span><b>' . __('Date:') . '</b>  <i>' . $data['event_time'] . '</i></span>';
+		$output .= '<br><span><b>' . __('Action:') . '</b>  <i>' . $data['action'] . '</i></span>';
+		$output .= '<hr>';
+		$output .= '<table style="width:100%">';
+
+		if (sizeof($nattribs) > 16) {
+			$columns = 2;
+			$output .= '<tr class="tableHeader"><th style="width:25%">' . __('Attrib') . '</th><th style="width:25%">' . __('Value') . '</th><th style="width:25%">' . __('Attrib') . '</th><th style="width:25%">' . __('Value') . '</th></tr>';
+		}else{
+			$columns = 1;
+			$output .= '<tr class="tableHeader"><th style="width:50%">' . __('Attrib') . '</th><th style="width:50%">' . __('Value') . '</th></tr>';
+		}
+
+		$i = 0;
+		if (sizeof($nattribs)) {
+			foreach($nattribs as $field => $content) {
+				if ($i % $columns == 0) {
+					$output .= ($output != '' ? '</tr>':'') . '<tr>';
+				}
+
+				if (is_array($content)) {
+					$output .= '<td style="font-weight:bold;white-space:nowrap;">' . $field . '</td><td">' . implode(',', $content) . '</td>';
+				}else{
+					$output .= '<td style="font-weight:bold;white-space:nowrap;">' . $field . '</td><td>' . $content . '</td>';
+				}
+
+				$i++;
+			}
+
+			if ($i % $columns > 0) {
+				$output . '<td></td><td></td></tr>';
+			}
+		}
+		$output .= '</table>';
+	}
+
+	$output .= '</td></tr></table>';
+
+	print $output;
+
+	break;
+default:
+	top_header();
+	audit_log();
+	bottom_footer();
+}
+
+function audit_purge() {
+	db_execute('TRUNCATE TABLE audit_log');
+
+	$_SESSION['audit_message'] = __('Audit Log Purged by %s', get_username($_SESSION['sess_user_id']));
+
+	cacti_log('NOTE: Audit Log Purged by ' . get_username($_SESSION['sess_user_id']), false, 'WEBUI');
+
+	raise_message('audit_message');
+}
+
+function audit_export_rows() {
+	process_request_vars();
+
+	/* form the 'where' clause for our main sql query */
+	if (get_request_var('filter') != '') {
+		$sql_where = "WHERE (page LIKE '%" . get_request_var('filter') . "%' 
+			OR post LIKE '%" .  get_request_var('filter') . "%')";
+	}else{
+		$sql_where = '';
+	}
+
+	if (get_request_var('event_page') != '-1') {
+		$sql_where .= ($sql_where != '' ? ' AND ':'WHERE ') . ' page = ' . db_qstr(get_request_var('event_page'));
+	}
+
+	if (!isempty_request_var('user_id') && get_request_var('user_id') > '-1') {
+		$sql_where .= ($sql_where != '' ? ' AND ':'WHERE ') . ' user_id = ' . get_request_var('user_id');
+	}
+
+	$events = db_fetch_assoc("SELECT audit_log.*, user_auth.username
+		FROM audit_log
+		LEFT JOIN user_auth
+		ON audit_log.user_id=user_auth.id
+		$sql_where");
+
+	if (sizeof($events)) {
+		header('Content-Disposition: attachment; filename=audit_export.csv');
+
+		print "page, user_id, username, action, ip_address, user_agent, event_time, post\n";
+
+		foreach($events as $event) {
+			$post = json_decode($event['post']);
+			$poster = '';
+			foreach($post as $var => $value) {
+				if (is_array($value)) {
+					$poster .= ($poster != '' ? '|':'') . $var . ':' . implode('%', $value);
+				}else{
+					$poster .= ($poster != '' ? '|':'') . $var . ':' . $value;
+				}
+			}
+
+			print 
+				$event['page']                   . ', '  .
+				$event['user_id']                . ', '  . 
+				get_username($event['user_id'])  . ', '  .
+				$event['action']                 . ', '  .
+				$event['ip_address']             . ', '  .
+				$event['user_agent']             . ', '  .
+				$event['event_time']             . ', ' .
+				$poster                          . "\n";
+		}
+	}
+}
+
+function audit_csv_escape($string) {
+	$string = str_replace('"', '', $string);
+	$string = str_replace(',', '|', $string);
+	return $string;
+}
+
+function process_request_vars() {
+	/* ================= input validation and session storage ================= */
+	$filters = array(
+		'rows' => array(
+			'filter' => FILTER_VALIDATE_INT, 
+			'pageset' => true,
+			'default' => '-1'
+			),
+		'page' => array(
+			'filter' => FILTER_VALIDATE_INT, 
+			'default' => '1'
+			),
+		'filter' => array(
+			'filter' => FILTER_CALLBACK, 
+			'pageset' => true,
+			'default' => '', 
+			'options' => array('options' => 'sanitize_search_string')
+			),
+		'sort_column' => array(
+			'filter' => FILTER_CALLBACK, 
+			'default' => 'event_time', 
+			'options' => array('options' => 'sanitize_search_string')
+			),
+		'sort_direction' => array(
+			'filter' => FILTER_CALLBACK, 
+			'default' => 'DESC', 
+			'options' => array('options' => 'sanitize_search_string')
+			),
+		'user_id' => array(
+			'filter' => FILTER_VALIDATE_INT, 
+			'pageset' => true,
+			'default' => '-1'
+			),
+		'event_page' => array(
+			'filter' => FILTER_CALLBACK, 
+			'pageset' => true,
+			'default' => '-1', 
+			'options' => array('options' => 'sanitize_search_string')
+			)
+	);
+
+	validate_store_request_vars($filters, 'sess_color');
+	/* ================= input validation ================= */
+}
+
+function audit_log() {
+	global $item_rows;
+
+	process_request_vars();
+
+	if (get_request_var('rows') == '-1') {
+		$rows = read_config_option('num_rows_table');
+	}else{
+		$rows = get_request_var('rows');
+	}
+
+	html_start_box(__('Audit Log'), '100%', '', '3', 'center', '');
+
+	?>
+	<tr class='even'>
+		<td>
+			<form id='form_audit' action='audit.php'>
+			<table class='filterTable'>
+				<tr>
+					<td>
+						<?php print __('Search');?>
+					</td>
+					<td>
+						<input id='filter' type='text' name='filter' size='25' value='<?php print htmlspecialchars(get_request_var('filter'));?>'>
+					</td>
+					<td>
+						<?php print __('Page');?>
+					</td>
+					<td>
+						<select id='event_page'>
+							<option value='-1'<?php print (get_request_var('event_page') == '-1' ? ' selected>':'>') . __('All');?></option>
+							<?php
+							$pages = array_rekey(db_fetch_assoc('SELECT DISTINCT page FROM audit_log ORDER BY page'), 'page', 'page');
+							if (sizeof($pages)) {
+								foreach ($pages as $page) {
+									print "<option value='" . $page . "'"; if (get_request_var('event_page') == $page) { print ' selected'; } print '>' . htmlspecialchars($page) . "</option>\n";
+								}
+							}
+							?>
+						</select>
+					</td>
+					<td>
+						<?php print __('User');?>
+					</td>
+					<td>
+						<select id='user_id'>
+							<option value='-1'<?php print (get_request_var('user_id') == '-1' ? ' selected>':'>') . __('All');?></option>
+							<?php
+							$users = array_rekey(db_fetch_assoc('SELECT DISTINCT user_id FROM audit_log ORDER BY user_id'), 'user_id', 'user_id');
+							if (sizeof($users)) {
+								foreach ($users as $user) {
+									print "<option value='" . $user . "'"; if (get_request_var('user_id') == $user) { print ' selected'; } print '>' . htmlspecialchars(get_username($user)) . "</option>\n";
+								}
+							}
+							?>
+						</select>
+					<td>
+						<?php print __('Events');?>
+					</td>
+					<td>
+						<select id='rows'>
+							<option value='-1'<?php print (get_request_var('rows') == '-1' ? ' selected>':'>') . __('Default');?></option>
+							<?php
+							if (sizeof($item_rows)) {
+								foreach ($item_rows as $key => $value) {
+									print "<option value='" . $key . "'"; if (get_request_var('rows') == $key) { print ' selected'; } print '>' . htmlspecialchars($value) . "</option>\n";
+								}
+							}
+							?>
+						</select>
+					</td>
+					<td>
+						<input type='button' id='refresh' value='<?php print __('Go');?>' title='<?php print __('Set/Refresh Filters');?>'>
+					</td>
+					<td>
+						<input type='button' id='clear' value='<?php print __('Clear');?>' title='<?php print __('Clear Filters');?>'>
+					</td>
+					<td>
+						<input type='button' id='export' value='<?php print __('Export');?>' title='<?php print __('Export Log Events');?>'>
+					</td>
+					<td>
+						<input type='button' id='purge' value='<?php print __('Purge');?>' title='<?php print __('Purge Log Events');?>'>
+					</td>
+				</tr>
+				</tr>
+			</table>
+			<input type='hidden' id='page' value='<?php print get_request_var('page');?>'>
+			</form>
+			<script type='text/javascript'>
+			function applyFilter() {
+				strURL = 'audit.php' + 
+					'?filter='+$('#filter').val()+
+					'&rows='+$('#rows').val()+
+					'&page='+$('#page').val()+
+					'&event_page='+$('#event_page').val()+
+					'&user_id='+$('#user_id').val()+
+					'&header=false';
+				loadPageNoHeader(strURL);
+			}
+
+			function clearFilter() {
+				strURL = 'audit.php?clear=1&header=false';
+				loadPageNoHeader(strURL);
+			}
+
+			$(function() {
+				$('#event_page, #user_id, #rows').change(function() {
+					applyFilter();
+				});
+
+				$('#refresh').click(function() {
+					applyFilter();
+				});
+
+				$('#clear').click(function() {
+					clearFilter();
+				});
+
+				$('#purge').click(function() {
+					strURL = 'audit.php?action=purge&header=false';
+					loadPageNoHeader(strURL);
+				});
+
+				$('#export').click(function() {
+					document.location = 'audit.php?action=export' +
+						'&filter='+$('#filter').val()+
+						'&event_page='+$('#event_page').val()+
+						'&user_id='+$('#user_id').val();
+				});
+
+				$('#form_audit').submit(function(event) {
+					event.preventDefault();
+					applyFilter();
+				});
+			});
+			</script>
+		</td>
+	</tr>
+	<?php
+
+	html_end_box();
+
+	/* form the 'where' clause for our main sql query */
+	if (get_request_var('filter') != '') {
+		$sql_where = "WHERE (page LIKE '%" . get_request_var('filter') . "%' 
+			OR post LIKE '%" .  get_request_var('filter') . "%')";
+	}else{
+		$sql_where = '';
+	}
+
+	if (get_request_var('event_page') != '-1') {
+		$sql_where .= ($sql_where != '' ? ' AND ':'WHERE ') . ' page = ' . db_qstr(get_request_var('event_page'));
+	}
+
+	if (!isempty_request_var('user_id') && get_request_var('user_id') > '-1') {
+		$sql_where .= ($sql_where != '' ? ' AND ':'WHERE ') . ' user_id = ' . get_request_var('user_id');
+	}
+
+	$total_rows = db_fetch_cell("SELECT
+		COUNT(*)
+		FROM audit_log
+		LEFT JOIN user_auth
+		ON audit_log.user_id=user_auth.id
+		$sql_where");
+
+	$events = db_fetch_assoc("SELECT audit_log.*, user_auth.username
+		FROM audit_log
+		LEFT JOIN user_auth
+		ON audit_log.user_id=user_auth.id
+		$sql_where
+		ORDER BY " . get_request_var('sort_column') . ' ' . get_request_var('sort_direction') . "
+		LIMIT " . ($rows*(get_request_var('page')-1)) . ',' . $rows);
+
+    $nav = html_nav_bar('audit.php?filter=' . get_request_var('filter'), MAX_DISPLAY_PAGES, get_request_var('page'), $rows, $total_rows, 5, __('Audit Events'), 'page', 'main');
+
+    print $nav;
+
+	html_start_box('', '100%', '', '3', 'center', '');
+
+	$display_text = array(
+		'page' => array(
+			'display' => __('Page Name'),
+			'align' => 'left',
+			'sort' => 'ASC',
+			'tip' => __('The page where the event was generated.')
+		),
+		'user' => array(
+			'display' => __('User Name'),
+			'align' => 'left',
+			'sort' => 'ASC',
+			'tip' => __('The user who generated the event.')
+		),
+		'action' => array(
+			'display' => __('Action'),
+			'align' => 'left',
+			'sort' => 'ASC',
+			'tip' => __('The Cacti Action requested.  Hover over action to see $_POST data.')
+		),
+		'user_agent'  => array(
+			'display' => __('User Agent'),
+			'align' => 'left',
+			'sort' => 'ASC',
+			'tip' => __('The browser type of the requester.')
+		),
+		'ip_address'  => array(
+			'display' => __('IP Address'),
+			'align' => 'right',
+			'sort' => 'ASC',
+			'tip' => __('The IP Address of the requester.')
+		),
+		'event_time'  => array(
+			'display' => __('Event Time'),
+			'align' => 'right',
+			'sort' => 'DESC',
+			'tip' => __('The time the Event took place.')
+		)
+	);
+
+	html_header_sort($display_text, get_request_var('sort_column'), get_request_var('sort_direction'), false);
+
+	$i = 0;
+	if (sizeof($events)) {
+		foreach ($events as $e) {
+			form_alternate_row('line' . $e['id'], false);
+			form_selectable_cell(filter_value($e['page'], get_request_var('filter')), $e['id']);
+			form_selectable_cell($e['username'], $e['id']);
+			form_selectable_cell('<span id="event' . $e['id'] . '" class="linkEditMain">' . ucfirst($e['action']) . '</span>', $e['id']);
+			form_selectable_cell($e['user_agent'], $e['id']);
+			form_selectable_cell($e['ip_address'], $e['id'], '', 'right');
+			form_selectable_cell($e['event_time'], $e['id'], '', 'right');
+			form_end_row();
+		}
+	}else{
+		print "<tr class='tableRow'><td colspan='5'><em>" . __('No Audit Log Events Found') . "</em></td></tr>\n";
+	}
+
+	html_end_box(false);
+
+	if (sizeof($events)) {
+		print $nav;
+	}
+
+	?>
+	<script type='text/javascript'>
+	$('span[id^="event"]').hover(function() {
+		close_dialog();
+
+		id = $(this).attr('id').replace('event', '');
+		$.get('audit.php?action=getdata&id='+id, function(data) {
+			if (data.indexOf('narrow') > 0) {
+				width = 200;
+			}else{
+				width = 600;
+			}
+			$('body').append('<div id="audit" style="display:block;display:none;" title="<?php print __('Audit Event Details');?>">'+data+'</div>');
+			$('#audit').dialog({
+				minWidth: width,
+				position: {
+					my: 'left', 
+					at: 'right', 
+					of: $('span[id="event'+id+'"]')
+				}
+			});
+		});
+	},
+	function() {
+		close_dialog();
+	});
+
+	function close_dialog() {
+		if ($('#audit').length) {
+			if (typeof $('#audit').dialog() === 'function') {
+				$('#audit').dialog('close');
+			}
+			$('#audit').remove();
+		}
+	}
+	</script>
+	<?php
+}
+
---
+++ setup.php
@@ -0,0 +1,286 @@
+<?php
+/*
+ +-------------------------------------------------------------------------+
+ | Copyright (C) 2004-2017 The Cacti Group                                 |
+ |                                                                         |
+ | This program is free software; you can redistribute it and/or           |
+ | modify it under the terms of the GNU General Public License             |
+ | as published by the Free Software Foundation; either version 2          |
+ | of the License, or (at your option) any later version.                  |
+ |                                                                         |
+ | This program is distributed in the hope that it will be useful,         |
+ | but WITHOUT ANY WARRANTY; without even the implied warranty of          |
+ | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the           |
+ | GNU General Public License for more details.                            |
+ +-------------------------------------------------------------------------+
+ | Cacti: The Complete RRDTool-based Graphing Solution                     |
+ +-------------------------------------------------------------------------+
+ | This code is designed, written, and maintained by the Cacti Group. See  |
+ | about.php and/or the AUTHORS file for specific developer information.   |
+ +-------------------------------------------------------------------------+
+ | http://www.cacti.net/                                                   |
+ +-------------------------------------------------------------------------+
+*/
+
+function plugin_audit_install() {
+	api_plugin_register_hook('audit', 'config_arrays',        'audit_config_arrays',        'setup.php');
+	api_plugin_register_hook('audit', 'config_settings',      'audit_config_settings',      'setup.php');
+	api_plugin_register_hook('audit', 'config_insert',        'audit_config_insert',        'setup.php');
+	api_plugin_register_hook('audit', 'poller_bottom',        'audit_poller_bottom',        'setup.php');
+	api_plugin_register_hook('audit', 'draw_navigation_text', 'audit_draw_navigation_text', 'setup.php');
+	api_plugin_register_hook('audit', 'utilities_array',      'audit_utilities_array',      'setup.php');
+
+	api_plugin_register_realm('audit', 'audit.php', __('View Cacti Audit Log'), 1);
+
+	audit_setup_table();
+}
+
+function plugin_audit_uninstall() {
+	db_execute('DROP TABLE IF EXISTS audit_log');
+	return true;
+}
+
+function plugin_audit_check_config() {
+	return true;
+}
+
+function plugin_audit_upgrade() {
+	return true;
+}
+
+function audit_check_upgrade() {
+	global $config, $database_default;
+	include_once($config['library_path'] . '/database.php');
+	include_once($config['library_path'] . '/functions.php');
+
+	$files = array('plugins.php', 'audit.php');
+	if (isset($_SERVER['PHP_SELF']) && !in_array(basename($_SERVER['PHP_SELF']), $files)) {
+		return;
+	}
+
+	$version = plugin_audit_version ();
+	$current = $version['version'];
+	$old     = db_fetch_cell("SELECT version FROM plugin_config WHERE directory='audit'");
+	if ($current != $old) {
+		if (api_plugin_is_enabled('audit')) {
+			# may sound ridiculous, but enables new hooks
+			api_plugin_enable_hooks('audit');
+		}
+
+		db_execute("UPDATE plugin_config 
+			SET version='$current' 
+			WHERE directory='audit'");
+
+		db_execute("UPDATE plugin_config SET 
+			version='" . $version['version']  . "', 
+			name='"    . $version['longname'] . "', 
+			author='"  . $version['author']   . "', 
+			webpage='" . $version['url']      . "' 
+			WHERE directory='" . $version['name'] . "' ");
+	}
+}
+
+function audit_check_dependencies() {
+	return true;
+}
+
+function audit_poller_bottom() {
+	$last_check = read_config_option('audit_last_check');
+
+	$now = date('d');
+
+	if ($last_check != $now) {
+		$retention = read_config_option('audit_retention');
+
+		if ($retention > 0) {
+			db_execute('DELETE FROM audit_log WHERE event_time < FROM_UNIXTIME(' . (time() - ($retention * 86400)) . ')');
+			$rows = db_affected_rows();
+			cacti_log('NOTE: Purged ' . $rows . ' Audit Log Records from Cacti', false, 'POLLER');
+		}
+	}
+
+	set_config_option('audit_last_check', $now);
+}
+
+function audit_setup_table() {
+	global $config, $database_default;
+	include_once($config['library_path'] . '/database.php');
+
+	db_execute("CREATE TABLE IF NOT EXISTS `audit_log` (
+		`id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+		`page` varchar(40) DEFAULT NULL,
+		`user_id` int(10) unsigned DEFAULT NULL,
+		`action` varchar(20) DEFAULT NULL,
+		`ip_address` varchar(40) DEFAULT NULL,
+		`user_agent` varchar(256) DEFAULT NULL,
+		`event_time` timestamp DEFAULT CURRENT_TIMESTAMP,
+		`post` longblob,
+		PRIMARY KEY (`id`),
+		KEY `user_id` (`user_id`),
+		KEY `page` (`page`),
+		KEY `ip_address` (`ip_address`),
+		KEY `event_time` (`event_time`),
+		KEY `action` (`action`)) 
+		ENGINE=InnoDB 
+		COMMENT='Audit Log for all GUI activities'");
+
+	return true;
+}
+
+function plugin_audit_version () {
+	global $config;
+	$info = parse_ini_file($config['base_path'] . '/plugins/audit/INFO', true);
+	return $info['info'];
+}
+
+function audit_log_valid_event() {
+	global $action;
+
+	$valid = false;
+
+	if (read_config_option('audit_enabled') == 'on') {
+		if (strpos($_SERVER['SCRIPT_NAME'], 'graph_view.php') !== false) {
+			$valid = false;
+		}elseif (strpos($_SERVER['SCRIPT_NAME'], 'user_admin.php') !== false && 
+			isset_request_var('action') && get_nfilter_request_var('action') == 'checkpass') {
+			$valid = false;
+		}elseif (strpos($_SERVER['SCRIPT_NAME'], 'plugins.php') !== false) {
+			if (isset_request_var('mode')) {
+				$valid  = true;
+				$action = $_REQUEST['mode'];
+			}
+		}elseif (strpos($_SERVER['SCRIPT_NAME'], 'auth_profile.php') !== false) {
+			$valid = false;
+		}elseif (isset($_POST) && sizeof($_POST)) {
+			$valid = true;
+		}elseif (isset_request_var('purge_continue')) {
+			$valid  = true;
+			$action = 'purge';
+		}
+	}
+	
+	return $valid;
+}
+
+function audit_config_insert() {
+	global $action;
+
+	if (audit_log_valid_event()) {
+		/* prepare post */
+		$post        = $_REQUEST;
+
+		/* remove unsafe vairables */
+		if (isset($post['__csrf_magic'])) {
+			unset($post['__csrf_magic']);
+		}
+
+		if (isset($post['header'])) {
+			unset($post['header']);
+		}
+
+		if (isset($post['password'])) {
+			unset($post['password']);
+		}
+
+		if (isset($post['password_confirm'])) {
+			unset($post['password_confirm']);
+		}
+
+		$post        = json_encode($post);
+		$page        = basename($_SERVER['SCRIPT_NAME']);
+		$user_id     = $_SESSION['sess_user_id'];
+		$event_time  = date('Y-m-d H:i:s');
+		$ip_address  = $_SERVER['REMOTE_ADDR'];
+		$user_agent  = $_SERVER['HTTP_USER_AGENT'];
+
+		if (empty($action) && isset_request_var('action')) {
+			$action = get_nfilter_request_var('action');
+		}elseif(empty($action)) {
+			$action = 'none';
+		}
+
+		db_execute_prepared('INSERT INTO audit_log (page, user_id, action, ip_address, user_agent, event_time, post)
+			VALUES (?, ?, ?, ?, ?, ?, ?)',
+			array($page, $user_id, $action, $ip_address, $user_agent, $event_time , $post));
+	}
+}
+
+function audit_utilities_array() {
+	global $utilities;
+
+	/* append technical support page */
+	$utilities[__('Technical Support')] = array_merge(
+		$utilities[__('Technical Support')], 
+		array(
+			__('View Audit Log') => array(
+				'link'  => 'plugins/audit/audit.php',
+				'description' => __('Allows Administrators to view change activity on the Cacti server.  Administrators can also export the audit log for analysis purposes.')
+			)
+		)
+	);
+}
+
+function audit_config_arrays() {
+	global $menu, $messages, $audit_retentions, $utilities;
+
+	if (isset($_SESSION['audit_message']) && $_SESSION['audit_message'] != '') {
+		$messages['audit_message'] = array('message' => $_SESSION['audit_message'], 'type' => 'info');
+	}
+
+	$audit_retentions = array(
+		-1   => __('Indefinitely'),
+		14   => __('%d Weeks',  2),
+		30   => __('%d Month',  1),
+		60   => __('%d Months', 2),
+		90   => __('%d Months', 3),
+		120  => __('%d Months', 4),
+		183  => __('%d Months', 6),
+		365  => __('%d Year',   1),
+		730  => __('%d Years',  2),
+		1095 => __('%d Years',  3)
+	);
+}
+
+function audit_config_settings () {
+	global $tabs, $settings, $item_rows, $audit_retentions;
+
+	$temp = array(
+		'audit_header' => array(
+			'friendly_name' => __('Audit Log Settings'),
+			'method' => 'spacer',
+			),
+		'audit_enabled' => array(
+			'friendly_name' => __('Enable Audit Log'),
+			'description' => __('Check this box, if you want the Audit Log to track GUI activities.'),
+			'method' => 'checkbox',
+			'default' => 'on'
+			),
+		'audit_retention' => array(
+			'friendly_name' => __('Audit Log Retention'),
+			'description' => __('How long do you wish Audit Log entries to be retained?'),
+			'method' => 'drop_array',
+			'default' => '90',
+			'array' => $audit_retentions
+			),
+		);
+
+	$tabs['misc'] = __('Misc');
+
+	if (isset($settings['misc'])) {
+		$settings['misc'] = array_merge($settings['misc'], $temp);
+	}else{
+		$settings['misc'] = $temp;
+	}
+}
+
+function audit_draw_navigation_text($nav) {
+	$nav['audit.php:'] = array(
+		'title'   => __('Audit Event Log'), 
+		'mapping' => '', 
+		'url'     => 'audit.php', 
+		'level'   => '0'
+	);
+
+	return $nav;
+}
+
